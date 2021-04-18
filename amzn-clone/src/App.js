ca507cdb418741521b90273916174a84f1d5f572
rivetin <athulofficial01@gmail.com>
2021-04-18 21:12:22 +0530

    routes

--- amzn-clone/src/App.js
+++ amzn-clone/src/App.js
@@ -2,13 +2,24 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 
   );
 }
--- amzn-clone/src/Product.js
+++ amzn-clone/src/Product.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
+
     return (
-        <div className='product'>
+        <div className="product">
             <div className="product__info">
-                <p>The lean Startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
-
                 <div className="product__rating">
-                    <p>✡</p>
-                    <p>✡</p>
-                    <p>✡</p>
-                    <p>✡</p>
-                    <p>✡</p>
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>✡</p>
+                        ))}
                 </div>
-
-                <img src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" alt="" />
-                <button>Add to basket</button>
-
             </div>
 
+            <img src={image} alt="" />
+
+            <button>Add to Basket</button>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
