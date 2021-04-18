5e187868d6bb0232ee8c99efae6838a27809ee59
rivetin <athulofficial01@gmail.com>
2021-04-18 11:31:43 +0530

    initial

---
+++ amzn-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCpvOv3tMPslz60kcix6z7ROUwZ5OAIacw",
+    authDomain: "amzn-clone-ecb7b.firebaseapp.com",
+    projectId: "amzn-clone-ecb7b",
+    storageBucket: "amzn-clone-ecb7b.appspot.com",
+    messagingSenderId: "366941717588",
+    appId: "1:366941717588:web:3747e7daf861ac450759c1",
+    measurementId: "G-BMBK9TKNY4"
+};
